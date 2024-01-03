fe60a44076e85eaca4749688edd3e4be28d53cdd
Cristian Riffo Huez <criffoh@gmail.com>
2024-01-02 22:07:57 -0300

    Update Modules.php
    parche para each()

--- application/third_party/MX/Modules.php
+++ application/third_party/MX/Modules.php
@@ -79,7 +79,7 @@ class Modules
 	/** Load a module controller **/
 	public static function load($module) {
 
-		(is_array($module)) ? list($module, $params) = each($module) : $params = NULL;	
+		(is_array($module)) ? list($module, $params) = myEach($module) : $params = NULL;	
 		
 		/* get the requested controller class name */
 		$alias = strtolower(basename($module));
@@ -218,4 +218,12 @@ class Modules
 			}
 		}
 	}
+}
+
+// https://stackoverflow.com/questions/70001740/php-each-function-replacement
+function myEach(&$arr) {
+    $key = key($arr);
+    $result = ($key === null) ? false : [$key, current($arr), 'key' => $key, 'value' => current($arr)];
+    next($arr);
+    return $result;
 }
